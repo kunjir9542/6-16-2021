4439126ce2edcc4529c630080c859c092debe250
Aryan <aryank20987@gmail.com>
2021-06-16 20:01:20 -0500

    .

--- r_script.R
+++ r_script.R
@@ -1,4 +1,14 @@
 library(tidyverse)
 library(rvest)
+library(lubridate)
+raw_data <- read_html("https://en.wikipedia.org/wiki/UEFA_Euro_2020_squads")
 
-x <- read_html("https://en.wikipedia.org/wiki/UEFA_Euro_2020_squads")
+ raw_data <- raw_data %>%
+  html_elements("td:nth-child(4)") %>%
+  html_text() %>%
+  as_tibble()
+
+write_csv(raw_data, "birthdates.csv")
+
+ 
+  
